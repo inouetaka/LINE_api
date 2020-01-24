19a563a660d2ee020a9f0027d930f5910a616334
inouetaka <webroot63@gmail.com>
2020-01-24 17:37:34 +0900

    image send test

--- main.py
+++ main.py
@@ -51,16 +51,12 @@ def handle_message(event):
     poke = pd.read_csv('./poke.csv', index_col=0)
     result = poke[poke['name'] == f'{word},1,001']
     result = result.to_json(force_ascii=False)
-    type_ = result['type']
-    file = {"imageFile": open(type_, "rb")}
 
 
     line_bot_api.reply_message(
         event.reply_token,
-        #TextSendMessage(text=result)
-        ImageSendMessage(original_content_url=file)
+        TextSendMessage(text=result)
     )
-
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
