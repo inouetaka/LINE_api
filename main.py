8765baee70e6aab65dbfc676619ec90cde27fa5c
inouetaka <webroot63@gmail.com>
2020-01-24 11:47:02 +0900

    image send test

--- main.py
+++ main.py
@@ -48,18 +48,17 @@ def handle_message(event):
 def handle_message(event):
 
     word = event.message.text
-    poke_origin = pd.read_csv('./poke.csv')
-    poke = poke_origin.drop(["Unnamed: 0"], axis=1)
+    poke = pd.read_csv('./poke.csv', index_col=0)
     result = poke[poke['name'] == f'{word},1,001']
     result = result.to_json(force_ascii=False)
-    type_ = result['type']
-    file = {"imageFile": open(type_, "rb")}
+    #type_ = result['type']
+    #file = {"imageFile": open(type_, "rb")}
 
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=result),
-        ImageSendMessage(original_content_url=file)
+        TextSendMessage(text=result)
+        #ImageSendMessage(original_content_url=file)
     )
 
 
--- test.py
+++ test.py
@@ -2,14 +2,10 @@ import pandas as pd
 import json
 
 output = {}
-poke_origin = pd.read_csv('poke.csv')
-poke = poke_origin.drop(["Unnamed: 0"], axis=1)
+poke = pd.read_csv('poke.csv', index_col=0)
+print(poke)
 result = poke[poke['name'] == 'サルノリ,1,001']
 output['name'] = result["name"]
 output['type'] = result['type']
 output['img'] = result['img']
-
-
-j = json.dumps(output, default=object)
-print(j)
 
