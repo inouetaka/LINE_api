c85f165d53980599fb4e2db1886402239d34b94c
inouetaka <webroot63@gmail.com>
2020-01-24 18:13:50 +0900

    image send test

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ def handle_message(event):
     word = event.message.text
     poke = pd.read_csv('./poke.csv', index_col=0)
     result = poke[poke['name'] == word]
-    #result = result.to_json(force_ascii=False)
     #res = f"ポケモン画像:{result['type'][0]}\n図鑑番号:{result['number'][0]}\n名前:{result['name'][0]}"
 
     res = ImageSendMessage(
@@ -60,7 +59,8 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=res)
+        #TextSendMessage(text=res)
+        res
     )
 
 if __name__ == "__main__":
